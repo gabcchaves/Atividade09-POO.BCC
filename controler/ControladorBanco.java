d587a37f5c42716a27199fb4bdb28c361036587f
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-04 21:48:41 -0300

    Corrigi erros no controlador do banco.

--- controler/ControladorBanco.java
+++ controler/ControladorBanco.java
@@ -98,7 +98,9 @@ public class ControladorBanco {
         repositorioConta.alterarConta(conta2);
 
 		// Data, descrição, valor, tipo de transação
-		conta.setExtrato(LocalDate.now(), "Transferido para conta #" + destino, valor, "D");
+		// Registro nos extratos de ambas as contas
+		conta1.setExtrato(LocalDate.now(), "Transferido para conta #" + destino, valor, "D");
+		conta2.setExtrato(LocalDate.now(), "Recebido da conta #" + origem, valor, "C");
     }
 
     public ArrayList<String> gerarExtrato() {
