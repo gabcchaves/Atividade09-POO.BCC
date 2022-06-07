ec043fc6999137032db9ba0b93281627169a2aab
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-07 19:49:18 -0300

    ItemExtrato

--- model/conta/ItemExtrato.java
+++ model/conta/ItemExtrato.java
@@ -1,1 +1,11 @@
-public class ItemExtrato {}
+import java.time.LocalDate;
+
+public class ItemExtrato {
+    private LocalDate data;
+    private double saldoAnterior;
+    
+    public ItemExtrato() {
+        this.data = novaData;
+        this.saldoAnterior = novoSaldoAnterior;
+    }
+}
