e4c13a3a8a44c291b2d5624f0ea895c356bd8dfa
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-07 19:53:57 -0300

    Dois primeiros atributos.

--- model/conta/ItemExtrato.java
+++ model/conta/ItemExtrato.java
@@ -1,11 +1,26 @@
 import java.time.LocalDate;
 
+// Itens do extrato
 public class ItemExtrato {
     private LocalDate data;
     private double saldoAnterior;
     
-    public ItemExtrato() {
-        this.data = novaData;
+    public ItemExtrato(double saldoAnterior) {
         this.saldoAnterior = novoSaldoAnterior;
+    }
+    
+    // Definir data do item
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    
+    // Retornar data do item
+    public LocalDate getData() {
+        return this.data;
+    }
+    
+    // Retornar saldo anterior
+    public double getSaldoAnterior() {
+        return this.saldoAnterior;
     }
 }
